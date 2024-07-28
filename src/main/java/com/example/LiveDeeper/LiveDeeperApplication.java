3f163247539e0a9913a259a8291ec87d30adc863
Runek00 <runek00@gmail.com>
2024-07-29 00:10:41 +0200

    spring init

---
+++ src/main/java/com/example/LiveDeeper/LiveDeeperApplication.java
@@ -0,0 +1,13 @@
+package com.example.LiveDeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveDeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveDeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LiveDeeper/LiveDeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LiveDeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveDeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
